6c0f79e3d01c2a5d3da397bb2255935be7ceebf2
Damien Dart <damiendart@pobox.com>
2012-02-20 11:58:08 +0000

    Add a few comments.

--- bash-bits.sh
+++ bash-bits.sh
@@ -10,15 +10,18 @@ function setupVimAsDefaultEditor() {
   export EDITOR="$(type -P vim || type -P vi || echo "$EDITOR")"
   VISUAL="$(type -P mvim || type -P gvim || echo "$EDITOR")"
   [ -z "$DISPLAY" ] && VISUAL="$EDITOR"
+  # See <http://vimdoc.sourceforge.net/htmldoc/gui_x11.html#gui-fork>.
   [ "$VISUAL" = "$(type -P gvim)" ] && VISUAL="$VISUAL -f"
   [ "$VISUAL" = "$(type -P mvim)" ] && VISUAL="$VISUAL -f --nomru"
   export VISUAL
 }
 
+# The following function uses functions defined in "bash-completion.bash",
+# which is included with the Git source code distribution.
+# TODO: Add support for other SCMs and shells.
 function setupGitAwarePrompt() {
   [ -z "$PROMPT_COLOUR" ] && export PROMPT_COLOUR='\[\e[36;1m\]'
   local PROMPT='\w \$\[\e[0m\] '
-  # TODO: Add support for other SCMs.
   if [ "$(type -t __git_ps1)" ]; then
     export GIT_PS1_SHOWDIRTYSTATE=1
     export GIT_PS1_SHOWSTASHSTATE=1
@@ -26,5 +29,8 @@ function setupGitAwarePrompt() {
     PROMPT='\w$(__git_ps1 " [git:%s]" | sed -e "s/ //2") \$\[\e[0m\] '
   fi
   export PS1="${PROMPT_COLOUR}${PROMPT}"
+  # Fall back to a bare-bones prompt when using "sudo" to run commands as
+  # another user, as the bits and pieces required to display repository
+  # information in the prompt might not be available to the assumed user.
   export SUDO_PS1='\[\e[37;1;41m\]\w \$\[\e[0m\] '
 }
